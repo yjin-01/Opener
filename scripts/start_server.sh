eccac811ae76acf2d68bc3063ef1d01ee008c187
yjin-01 <t781797@gmail.com>
2024-01-22 17:26:09 +0900

    Chore: CD setting

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,7 +4,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
 
-npm install
-npm ci
+
 npm run build
 npm run start:prod
