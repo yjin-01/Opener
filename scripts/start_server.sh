805d0bad801763915e3aac645b178c536adbae31
yjin-01 <t781797@gmail.com>
2024-01-26 18:32:46 +0900

    Chore: CD test

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,7 +4,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
 
-npm install
-npm run build
-npm run start:prod
+cd /home/ubuntu/nest-app
 
+#npm run build
+#npm run start:prod
+
+pm2 start "npm run start:prod"
