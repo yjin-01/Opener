62880a798a2d254dc741efb06cb834d3146c81a8
dimsssss <caruen@hanmail.net>
2024-01-27 16:15:52 +0900

    Fix/user signup request (#57)
    * fix: 누락된 password 추가
    
    * refactor: 디버그용 console.log 삭제
    
    * refactor: request로 이름 수정
    
    * docs: 400에 대한 내용 추가

--- src/user/dto/user.signup.request.ts
+++ src/user/dto/user.signup.request.ts
@@ -1,9 +1,13 @@
 import {
-  IsUrl, IsOptional, IsString, IsEmail,
+  IsUrl,
+  IsOptional,
+  IsString,
+  IsEmail,
+  IsStrongPassword,
 } from 'class-validator';
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
-export class UserSignupDto {
+export class UserSignupRequest {
   @ApiProperty({ description: '회원 가입 유저 이름', default: 'test' })
   @IsString()
     userName: string;
@@ -16,6 +20,10 @@ export class UserSignupDto {
   @ApiProperty({ description: '이메일', default: 'test@test.com' })
   @IsEmail()
     email: string;
+
+  @ApiProperty({ description: '이메일', default: 'test@test.com' })
+  @IsStrongPassword({ minLength: 8 })
+    password: string;
 
   @ApiPropertyOptional({ description: '가입한 유저 닉네임', default: 'p1z7' })
   @IsOptional()
---
+++ src/user/swagger/user.signup.badrequest.ts
@@ -0,0 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserSignupBadRequest {
+  @ApiProperty({
+    description: '대략적인 실패 내용',
+    default: 'Validation failed',
+  })
+    message: string;
+
+  @ApiProperty({ description: 'http status 내용', default: 'Bad Request' })
+    error: string;
+
+  @ApiProperty({ description: 'http status code', default: 400 })
+    statusCode: number;
+}
