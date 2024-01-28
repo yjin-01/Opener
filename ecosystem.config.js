f98dff2983421de5cf59335748990e505cf1e13a
dimsssss <caruen@hanmail.net>
2024-01-29 00:32:58 +0900

    chore: main.yml 원복, execute.sh로 통합 (#59)
    * chore: main.yml 원복, execute.sh로 통합
    
    * chore: 중복 작업 주석처리
    
    * chore: instance 수 추가

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,7 @@ module.exports = {
        {
           name: 'p1z7',
           script: './dist/src/main.js',
-          instances: 1,
-          reload: true,
+          instances: 2,
           env: {
             Server_PORT: 3000,
             NODE_ENV: 'dev',
