cea1a7321559cb15f1f0044d4000f433cb03cf33
yjin-01 <t781797@gmail.com>
2024-01-30 11:49:52 +0900

    feat/artist&group 등록 조회 API

---
+++ src/artist/entity/artist_group.entity.ts
@@ -0,0 +1,41 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity('artist_groups')
+export class ArtistGroup {
+  @PrimaryGeneratedColumn('uuid', { name: 'artist_group_id' })
+    artistGroupId: string;
+
+  @Column({ name: 'artist_id' })
+    artistId: string;
+
+  @Column({ name: 'group_id' })
+    groupId: string;
+
+  @CreateDateColumn({
+    name: 'created_at',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+    createdAt: Date;
+
+  @UpdateDateColumn({
+    name: 'updated_at',
+    onUpdate: 'CURRENT_TIMESTAMP',
+    nullable: true,
+  })
+    updatedAt: Date;
+
+  @DeleteDateColumn({
+    name: 'deleted_at',
+    onUpdate: 'CURRENT_TIMESTAMP',
+    nullable: true,
+  })
+    deletedAt: Date;
+}
