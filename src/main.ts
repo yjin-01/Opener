b5c1b67ecbb62b8640930b86e6b8b0e8cea85813
yjin-01 <t781797@gmail.com>
2024-02-01 15:30:15 +0900

    Fix/swagger-token관련 수정

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,34 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import AppModule from './app.module';
 
+// 새로고침해도 토큰 유지 옵션
+const swaggerOptions = {
+  swaggerOptions: {
+    persistAuthorization: true, // 캐시 사용 활성화
+  },
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('API docs')
     .setDescription('The p1z7 API description.')
     .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'Bearer',
+        name: 'JWT',
+        in: 'header',
+      },
+      'accessToken',
+    )
     .addTag('p1z7')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, swaggerOptions);
   await app.listen(3000);
 }
 
