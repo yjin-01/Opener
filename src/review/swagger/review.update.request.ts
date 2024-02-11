0e78c98577c283b6ffd1ecd9a48c9357d58cbef7
dev <caruen@hanmail.net>
2024-02-11 09:30:32 +0900

    feat: api property optional 추가

--- src/review/swagger/review.update.request.ts
+++ src/review/swagger/review.update.request.ts
@@ -1,4 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
 export class ReviewUpdateRequest {
   @ApiProperty({ description: '리뷰를 작성한 유저 아이디', default: 'test' })
@@ -7,18 +7,24 @@ export class ReviewUpdateRequest {
   @ApiProperty({ description: '이 리뷰가 달린 이벤트 아이디', default: 'test' })
     eventId: string;
 
-  @ApiProperty({
+  @ApiProperty({ description: '리뷰 아이디', default: 'test' })
+    reviewId: string;
+
+  @ApiPropertyOptional({
     description: '리뷰 공개 여부',
     default: true,
   })
     isPublic: boolean;
 
-  @ApiProperty({
+  @ApiPropertyOptional({
     description: '이벤트에 대한 평가',
     default: true,
   })
     rating: boolean;
 
-  @ApiProperty({ description: '리뷰 내용', default: '한 번 더 갈 예정' })
+  @ApiPropertyOptional({
+    description: '리뷰 내용',
+    default: '한 번 더 갈 예정',
+  })
     description: string;
 }
