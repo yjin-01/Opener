9fe78e2b555a2cb4be4da50d95c24d019f686c3d
yjin-01 <t781797@gmail.com>
2024-02-21 18:38:37 +0900

    hotfix/image-upload

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as express from 'express';
 import AppModule from './app.module';
 
 // 새로고침해도 토큰 유지 옵션
@@ -29,6 +30,7 @@ async function bootstrap() {
   app.enableCors({
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 744d8fa (fix: allow origin all (#273))
     // origin: [
@@ -49,14 +51,21 @@ async function bootstrap() {
 >>>>>>> ab210df (fix: origin 추가 (#271) (#272))
 =======
 >>>>>>> 744d8fa (fix: allow origin all (#273))
+=======
+    origin: ['http://localhost:3000', 'localhost:3000'],
+>>>>>>> ef85026 (hotfix/image-upload)
     allowedHeaders: ['Authorization', 'Set-Cookie'],
     credentials: true,
     methods: ['GET', 'PATCH', 'POST', 'DELETE', 'PUT', 'HEAD', 'OPTIONS'],
   });
 
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('api', app, document, swaggerOptions);
+
   await app.listen(3000);
 }
 
